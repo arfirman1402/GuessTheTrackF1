b6775f5324edd1bdf2646ec5d39e67a368bdf6a2
arfirman1402 <ar.firman.syahputra@gmail.com>
2017-09-11 17:13:37 +0700

    Creating SplashActivity

---
+++ app/src/main/java/io/arfirman1402/dev/guessthetrackf1/SplashActivity.java
@@ -0,0 +1,17 @@
+package io.arfirman1402.dev.guessthetrackf1;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by alodokter-it on 11/09/17 -- SplashActivity.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
