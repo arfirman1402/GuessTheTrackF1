780bb308c9c2b59c5aef7150c353ef0c1865b008
arfirman1402 <ar.firman.syahputra@gmail.com>
2017-09-11 17:28:28 +0700

    Doing splash in SplashActivity

--- app/src/main/java/io/arfirman1402/dev/guessthetrackf1/SplashActivity.java
+++ app/src/main/java/io/arfirman1402/dev/guessthetrackf1/SplashActivity.java
@@ -1,6 +1,8 @@
 package io.arfirman1402.dev.guessthetrackf1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
@@ -13,5 +15,19 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                goToMain();
+            }
+        };
+        handler.postDelayed(runnable, 3000);
+    }
+
+    private void goToMain() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
